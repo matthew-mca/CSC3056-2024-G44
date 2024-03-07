889fad2b13897bde61c5155326b8a426316dd53f
Matthew McAteer <mmcateer18@qub.ac.uk>
2024-03-07 16:26:30 +0000

    Add ten randomly selected test cases for intersects method

--- Lab2/src/test/RangeTest.java
+++ Lab2/src/test/RangeTest.java
@@ -74,4 +74,86 @@ public class RangeTest extends TestCase {
             20.0, rangeObjectUnderTest.constrain(21.0)
         );
     }
+
+    // Tests for Range.intersects()
+
+    @Test
+    public void testIntersectsLowerValueAboveLowerBoundaryAndUpperValueBelowUpperBoundary() {
+        assertTrue(
+            "A range from -75 to 19 should overlap with a range from -76 to 20.",
+            rangeObjectUnderTest.intersects(-75.0, 19.0)
+        );
+    }
+
+    @Test
+    public void testIntersectsLowerValueAndUpperValueAboveUpperBoundary() {
+        assertFalse(
+            "A range from 21 to 21 should not overlap with a range from -76 to 20.",
+            rangeObjectUnderTest.intersects(21.0, 21.0)
+        );
+    }
+
+    @Test
+    public void testIntersectsLowerValueBelowLowerBoundaryAndUpperValueIsNominal() {
+        assertTrue(
+            "A range from -77 to 8 should overlap with a range from -76 to 20.",
+            rangeObjectUnderTest.intersects(-77.0, 8.0)
+        );
+    }
+
+    @Test
+    public void testIntersectsLowerValueBelowLowerBoundaryAndUpperValueOnLowerBoundary() {
+        assertTrue(
+            "A range from -77 to -76 should overlap with a range from -76 to 20.",
+            rangeObjectUnderTest.intersects(-77.0, -76.0)
+        );
+    }
+
+    @Test
+    public void testIntersectsLowerValueAndUpperValueOnUpperBoundary() {
+        assertTrue(
+            "A range from 20 to 20 should overlap with a range from -76 to 20.",
+            rangeObjectUnderTest.intersects(20.0, 20.0)
+        );
+    }
+
+    @Test
+    public void testIntersectsLowerValueBelowUpperBoundaryAndUpperValueBelowLowerBoundary() {
+        assertFalse(
+            "Lower value (19) and upper value (-77) are invalid for a range, so intersects() should return False.",
+            rangeObjectUnderTest.intersects(19.0, -77.0)
+        );
+    }
+
+    @Test
+    public void testLowerValueAndUpperValueBelowUpperBoundary() {
+        assertTrue(
+            "A range from 19 to 19 should overlap with a range from -76 to 20.",
+            rangeObjectUnderTest.intersects(19.0, 19.0)
+        );
+    }
+
+    @Test
+    public void testIntersectsLowerValueIsNominalAndUpperValueAboveLowerBoundary() {
+        assertFalse(
+            "Lower value (8) and upper value (-75) are invalid for a range, so intersects() should return False.",
+            rangeObjectUnderTest.intersects(8.0, -75.0)
+        );
+    }
+
+    @Test
+    public void testIntersectsLowerValueBelowLowerBoundaryAndUpperValueBelowUpperBoundary() {
+        assertTrue(
+            "A range from -77 to 19 should overlap with a range from -76 to 20.",
+            rangeObjectUnderTest.intersects(-77.0, 19.0)
+        );
+    }
+
+    @Test
+    public void testIntersectsLowerValueOnLowerBoundaryAndUpperValueOnUpperBoundary() {
+        assertTrue(
+            "A range from -76 to 20 should overlap with a range from -76 to 20.",
+            rangeObjectUnderTest.intersects(-76.0, 20.0)
+        );
+    }
 }
